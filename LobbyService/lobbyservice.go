ea5fc767d6f2aefb596d6a83f9e341538ab181a2
Faiq Sohail <me@faiqsohail.com>
2019-10-05 00:23:23 -0600

    Implemented function to allow creation of a lobby and added it's appropraite route.

--- LobbyService/lobbyservice.go
+++ LobbyService/lobbyservice.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -25,9 +27,28 @@ func lobbyService(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Current Version: 1.0.0")
 }
 
+func createLobby(w http.ResponseWriter, r *http.Request) {
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if err == nil {
+		var newLobby lobby
+		json.Unmarshal(reqBody, &newLobby)
+
+		newLobby.ID = len(lobbies) + 1 // automatically set the appropriate lobby ID
+		newLobby.Joinable = true       // a new lobby should be joinable
+		newLobby.CurrentPlayers = 0    // a new lobby should have no players in it by default
+
+		lobbies = append(lobbies, newLobby)
+		w.WriteHeader(http.StatusCreated)
+		json.NewEncoder(w).Encode(newLobby)
+	} else {
+		w.WriteHeader(http.StatusPreconditionFailed)
+	}
+}
+
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", lobbyService)
+	router.HandleFunc("/createlobby", createLobby).Methods("POST")
 	http.ListenAndServe(":8080", router)
 
 }
