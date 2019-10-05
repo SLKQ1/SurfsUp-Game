28d1992cf37b799abab544e80912145b21798a16
Faiq Sohail <me@faiqsohail.com>
2019-10-05 00:16:32 -0600

    Created representation of a lobby for the game.

--- LobbyService/lobbyservice.go
+++ LobbyService/lobbyservice.go
@@ -7,6 +7,19 @@ import (
 	"github.com/gorilla/mux"
 )
 
+// Represents a lobby room in the game
+type lobby struct {
+	ID             int    `json:"ID"`
+	GameType       string `json:"GameType"`       // The type of game the lobby is hosting, ex. Teams/Solo.
+	Joinable       bool   `json:"Joinable"`       // Whether or not the lobby is joinable
+	IsStarted      bool   `json:"IsStarted"`      // Whether or not the lobby is already started and the players in-game
+	CurrentPlayers int    `json:"CurrentPlayers"` // The current number of players in the lobby / game.
+	MaximumPlayers int    `json:"MaximumPlayers"` // The maximum number of players allowed in the lobby / game.
+}
+type allLobbies []lobby
+
+var lobbies = allLobbies{}
+
 func lobbyService(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Surfs Up! - Lobby Service")
 	fmt.Fprintf(w, "Current Version: 1.0.0")
