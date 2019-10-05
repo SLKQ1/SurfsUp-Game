494bb2edd03c8a5a01f738ce50d0f6c8b86b6cb5
Faiq Sohail <me@faiqsohail.com>
2019-10-05 13:16:55 -0600

    Modified player struct to allow for a secret token along with a seperate player ID, used to modify the player and to get the player respectively.

--- LobbyService/lobbyservice.go
+++ LobbyService/lobbyservice.go
@@ -23,9 +23,10 @@ type allLobbies []lobby
 
 // Represents players in the game
 type player struct {
-	ID          int    `json:"ID"`          // The ID of the player will serve as a secret between the player and the lobby service
-	PlayerName  string `json:"PlayerName"`  // The name of the player shown in-game
+	ID          int    `json:"ID"`
+	Token       string `json:"Token"`       // The Token of the player will serve as a secret between a player and the lobby service
 	LobbyID     int    `json:"LobbyID"`     // The lobby the player is currently joined. 0 if player is not in any lobby.
+	PlayerName  string `json:"PlayerName"`  // The name of the player shown in-game
 	PlayerTeam  string `json:"PlayerTeam"`  // The team name or team color the player is currently on.
 	PlayerReady bool   `json:"PlayerReady"` // Whether or not the player is ready to play whilst in a lobby.
 }
