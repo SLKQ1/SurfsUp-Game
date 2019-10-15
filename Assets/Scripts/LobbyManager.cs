adcf15975dc96b6eb8fdffd5ef3d8e0751fc4f49
Faiz Qureshi <faizqureshi111@gmail.com>
2019-10-14 18:58:11 -0600

    added flow functionality between screens by making buttons clickable

--- Assets/Scripts/LobbyManager.cs
+++ Assets/Scripts/LobbyManager.cs
@@ -5,29 +5,44 @@ using UnityEngine;
 public class LobbyManager : MonoBehaviour
 {
     public static LobbyManager Instance { set; get; }
+
+    // game objects for each screen on the menu
+    public GameObject mainMenu;
+    public GameObject lobbyListMenu;
+    public GameObject hostGameMenu;
+
     // Start is called before the first frame update
     private void Start()
     {
-        Instance = this; 
+        Instance = this;
+        lobbyListMenu.SetActive(false);
+        hostGameMenu.SetActive(false); 
         DontDestroyOnLoad(gameObject); 
-    }
-
-    // connect button
-    public void ConnectButton()
-    {
-        Debug.Log("Connect");
     }
 
     // host button
     public void HostButton()
     {
-        Debug.Log("Host"); 
+        Debug.Log("Host");
+        mainMenu.SetActive(false);
+        hostGameMenu.SetActive(true); 
     }
 
     // button to display lobbies
     public void LobbyListButton()
     {
         Debug.Log("list of lobbies");
+        mainMenu.SetActive(false);
+        lobbyListMenu.SetActive(true); 
+    }
+
+    // backbutton
+    public void BackButton()
+    {
+        mainMenu.SetActive(true);
+        lobbyListMenu.SetActive(false);
+        hostGameMenu.SetActive(false); 
+
     }
 
 
