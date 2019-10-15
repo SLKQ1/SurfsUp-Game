abb8ac5c3550a882cc90f381d5a87fc836586f80
Faiz Qureshi <faizqureshi111@gmail.com>
2019-10-14 19:51:34 -0600

    wrote basic scripts to generate buttons on the lobby list screen

---
+++ Assets/Scripts/ButtonListButton.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+public class ButtonListButton : MonoBehaviour
+{
+    [SerializeField]
+    private Text myText;
+    [SerializeField]
+    private ButtonListControl lobbyNum;
+
+    // string to hold text of button 
+    private string myTextString; 
+
+    // function to set button text
+    public void SetText(string textString)
+    {
+        this.gameObject.GetComponent<Button>().onClick.AddListener(OnClick); 
+        myTextString = textString; 
+        myText.text = textString; 
+    }
+
+    // function to provide logic on a click 
+    public void OnClick()
+    {
+        lobbyNum.ButtonClicked(myTextString); 
+    }
+}
---
+++ Assets/Scripts/ButtonListControl.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonListControl : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject buttonTemplate;
+
+    private void Start()
+    {
+        for (int i = 0; i < 20; i++)
+        {
+            // making button and making visiable
+            GameObject button = Instantiate(buttonTemplate) as GameObject;
+            button.SetActive(true);
+
+            // setting button text 
+            button.GetComponent<ButtonListButton>().SetText("Button #" + i);
+            // setting button position
+            button.transform.SetParent(buttonTemplate.transform.parent, false); 
+        }
+    }
+
+    public void ButtonClicked(string myTextString)
+    {
+        Debug.Log(myTextString);
+    }
+
+    // function to generate buttons
+    void GenButtons()
+    {
+
+    }
+
+}
