4fdb9ca5dcc647b0d525491eb8c3168d9efb28de
Faiz Qureshi <faq588@mail.usask.ca>
2019-10-14 18:31:14 -0600

    got a working layout for the lobby server list screen, will now work on making the scripts to generate the current active lobbies and make the UI work to display different canvas's when buttons are clicked.

---
+++ Assets/Scripts/LobbyManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LobbyManager : MonoBehaviour
+{
+    public static LobbyManager Instance { set; get; }
+    // Start is called before the first frame update
+    private void Start()
+    {
+        Instance = this; 
+        DontDestroyOnLoad(gameObject); 
+    }
+
+    // connect button
+    public void ConnectButton()
+    {
+        Debug.Log("Connect");
+    }
+
+    // host button
+    public void HostButton()
+    {
+        Debug.Log("Host"); 
+    }
+
+    // button to display lobbies
+    public void LobbyListButton()
+    {
+        Debug.Log("list of lobbies");
+    }
+
+
+}
