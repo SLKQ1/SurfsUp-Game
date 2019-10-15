f2aba7e6213c9ad77b3e293cb5fda0848943f56a
Faiz Qureshi <faq588@mail.usask.ca>
2019-10-15 10:28:05 -0600

    created a new canvas for joining a lobby, still need to add logic of creating a new player and adding that pplayer to the lobby

--- Assets/Scripts/ButtonListControl.cs
+++ Assets/Scripts/ButtonListControl.cs
@@ -9,11 +9,24 @@ public class ButtonListControl : MonoBehaviour
 
     private void Start()
     {
+        GenButtons(); 
 
+    }
+
+    // function to open lobby options when button is clicked 
+    public void ButtonClicked(string myTextString)
+    {
+        Debug.Log(myTextString);
+        // open the new canvas to show lobby joining options 
+    }
+
+    // function to generate buttons
+    void GenButtons()
+    {
         // testing to see if lobbies show
         JSONParser LobbyParser = new JSONParser();
-        List<LobbyInfo> LobbyList = LobbyParser.GetListLobbies(); 
-        Debug.Log("lobby: " + LobbyList[index:1].ID);
+        List<LobbyInfo> LobbyList = LobbyParser.GetListLobbies();
+        Debug.Log("lobby: " + LobbyList[index: 1].ID);
 
         // loop to create lobby buttons 
         for (int i = 0; i < LobbyList.Count; i++)
@@ -22,22 +35,12 @@ public class ButtonListControl : MonoBehaviour
             GameObject button = Instantiate(buttonTemplate) as GameObject;
             button.SetActive(true);
 
-            // setting button text
-            button.GetComponent<ButtonListButton>().SetText("Lobby ID: " + LobbyList[i].ID);
+            // setting button text to be the lobby id and if it is started or not  
+            button.GetComponent<ButtonListButton>().SetText("Lobby ID: " + LobbyList[i].ID + ", Is started: " + LobbyList[i].IsStarted);
             // setting button pos
-            button.transform.SetParent(buttonTemplate.transform.parent, false); 
+            button.transform.SetParent(buttonTemplate.transform.parent, false);
         }
 
-    }
-
-    public void ButtonClicked(string myTextString)
-    {
-        Debug.Log(myTextString);
-    }
-
-    // function to generate buttons
-    void GenButtons()
-    {
 
     }
 
