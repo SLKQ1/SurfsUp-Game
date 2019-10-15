dea95270f20570c1505d3133f0f28aab84be914f
Faiz Qureshi <faizqureshi111@gmail.com>
2019-10-14 23:19:26 -0600

    added functionality to the Lobby display screen so now it displays the currently active lobbies, still need to make the lobbies clickable and joinable

--- Assets/Scripts/ButtonListControl.cs
+++ Assets/Scripts/ButtonListControl.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using Newtonsoft.Json;  
 public class ButtonListControl : MonoBehaviour
 {
     [SerializeField]
@@ -9,17 +9,25 @@ public class ButtonListControl : MonoBehaviour
 
     private void Start()
     {
-        for (int i = 0; i < 20; i++)
+
+        // testing to see if lobbies show
+        JSONParser LobbyParser = new JSONParser();
+        List<LobbyInfo> LobbyList = LobbyParser.GetListLobbies(); 
+        Debug.Log("lobby: " + LobbyList[index:1].ID);
+
+        // loop to create lobby buttons 
+        for (int i = 0; i < LobbyList.Count; i++)
         {
-            // making button and making visiable
+            // making button and making visible
             GameObject button = Instantiate(buttonTemplate) as GameObject;
             button.SetActive(true);
 
-            // setting button text 
-            button.GetComponent<ButtonListButton>().SetText("Button #" + i);
-            // setting button position
+            // setting button text
+            button.GetComponent<ButtonListButton>().SetText("Lobby ID: " + LobbyList[i].ID);
+            // setting button pos
             button.transform.SetParent(buttonTemplate.transform.parent, false); 
         }
+
     }
 
     public void ButtonClicked(string myTextString)
--- Assets/Scripts/JSONParser.cs
+++ Assets/Scripts/JSONParser.cs
@@ -1,18 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Net;
+using Newtonsoft.Json;
 
-public class JSONParser : MonoBehaviour
+// class to parse json and return relevent information
+public class JSONParser 
 {
-    // Start is called before the first frame update
-    void Start()
+    // url for the API 
+    private string url = "http://lobbyservice.mooo.com:8080/";
+
+    // function to convert JSON string into list of LobbyInfo objects
+    public List<LobbyInfo> GetListLobbies()
     {
-        
+        var json = new WebClient().DownloadString(url + "/lobbies");
+        var LobbyList = JsonConvert.DeserializeObject<List<LobbyInfo>>(json);
+        return LobbyList; 
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
