3ecad88c9f22f3ca43b7ff7e46a233880246ef11
Faiq Sohail <me@faiqsohail.com>
2019-10-29 16:07:19 -0600

    Modified API Endpoints to keep them more consistent

--- LobbyService/lobbyservice.go
+++ LobbyService/lobbyservice.go
@@ -236,13 +236,14 @@ func idExist(id, length int) bool {
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", lobbyService)
-	router.HandleFunc("/createlobby", createLobby).Methods("POST")
-	router.HandleFunc("/createplayer", createPlayer).Methods("POST")
 	router.HandleFunc("/lobbies", getAllLobbies).Methods("GET")
 	router.HandleFunc("/lobbies/{id}", getLobbyByID).Methods("GET")
-	router.HandleFunc("/getserver/{id}", getServerByLobbyID).Methods("GET")
+	router.HandleFunc("/lobbies/create", createLobby).Methods("POST")
+	router.HandleFunc("/players/create", createPlayer).Methods("POST")
 	router.HandleFunc("/players/{id}", getPlayerByID).Methods("GET")
 	router.HandleFunc("/players/{id}", updatePlayerByID).Methods("PATCH")
+	router.HandleFunc("/players/create", createPlayer).Methods("POST")
+	router.HandleFunc("/servers/{id}", getServerByLobbyID).Methods("GET")
 	http.ListenAndServe(":8080", router)
 
 }
