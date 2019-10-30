84726b9d56dda12e2d881699e39f571210d83612
Faiq Sohail <me@faiqsohail.com>
2019-10-30 17:08:03 -0600

    Created PortInUse() which determines whether or not a give port is already binded

--- LobbyService/serverutils/port.go
+++ LobbyService/serverutils/port.go
@@ -2,13 +2,21 @@ package serverutils
 
 import (
 	"net"
+	"strconv"
 )
 
 // GeneratePort() generates a unused port number
 // which can be binded to by a server
 func GeneratePort() int {
-	// :0 Ask's Kernal to automatically bind to a unused port
+	// specifying port 0 is asking the Kernal to automatically bind to a unused port
 	listen, _ := net.Listen("tcp", ":0")
 	listen.Close()                           // Close listener
 	return listen.Addr().(*net.TCPAddr).Port // Return the port given by the Kernal
+}
+
+// PortInUse() returns whether or not a given port is already binded
+// to a service on the current system.
+func PortInUse(port int) bool {
+	_, err := net.Listen("tcp", ":"+strconv.Itoa(port))
+	return err != nil
 }
