5b4c6af2a3c77618355a05abfa64f11656769b36
Faiz Qureshi <faizqureshi111@gmail.com>
2019-11-09 14:53:11 -0700

    added a create lobby canvas, still need to do functionality

---
+++ Assets/Scripts/LobbyUIScripts/JoinLobbyButton.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JoinLobbyButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //void Start()
+    //{
+        
+    //}
+
+    public void OnClick()
+    {
+        Debug.Log("Join Button was clicked");
+
+    }
+}
