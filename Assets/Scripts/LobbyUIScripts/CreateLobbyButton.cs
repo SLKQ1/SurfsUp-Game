6a73e0698a3deaf62d5ac22d2081aa74691673d8
Faiz Qureshi <faq588@mail.usask.ca>
2019-11-12 14:27:33 -0700

    Can now send a post request when creating a new lobby but still need to update and show new lobby in lobby list

---
+++ Assets/Scripts/LobbyUIScripts/CreateLobbyButton.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+public class CreateLobbyButton : MonoBehaviour
+{
+    List<string> gameTypeOptions = new List<string>() { "Please select", "Solo", "Multiplayer" };
+    List<string> joinableOptions = new List<string>() { "Please select", "true", "false" };
+
+    public InputField MaxPlayers;
+    public Dropdown gameTypeDropdown;
+    public Dropdown joinableDropdown;
+
+    // vars for the selected options
+    public string gameType;
+    public string isJoinable;
+    //string maxPlayers;
+
+
+    public void Dropdown_IndexChanged_GameType(int index)
+    {
+        Debug.Log(gameTypeOptions[index]);
+        gameType = gameTypeOptions[index];
+    }
+    public void Dropdown_IndexChanged_JoinableOptions(int index)
+    {
+        Debug.Log(joinableOptions[index]);
+        isJoinable = joinableOptions[index]; 
+    }
+
+    private void Start()
+    {
+        PopulateLists();
+    }
+
+    void PopulateLists()
+    {
+        gameTypeDropdown.AddOptions(gameTypeOptions);
+        joinableDropdown.AddOptions(joinableOptions);
+    }
+
+    public void OnClick()
+    {
+        //Debug.Log(MaxPlayers.text);
+        CreateLobbyPOST newLobby = new CreateLobbyPOST();
+        //Debug.Log(gameType + " " + isJoinable + " " + MaxPlayers.text); 
+        newLobby.CreateLobby(gameType, isJoinable, Int32.Parse(MaxPlayers.text));
+        Debug.Log("Lobby created"); 
+    }
+
+
+}
---
+++ Assets/Scripts/LobbyUIScripts/CreateLobbyPOST.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using UnityEngine;
+using UnityEngine.UI; 
+
+public class CreateLobbyPOST : MonoBehaviour
+{
+    InputField MaximumPlayers; 
+
+    public void CreateLobby(string gameType, string isJoinable, int maxPlayers)
+    {
+        try
+        {
+            string webAddr = "http://lobbyservice.mooo.com:8080/lobbies/create";
+
+            var httpWebRequest = (HttpWebRequest)WebRequest.Create(webAddr);
+            httpWebRequest.ContentType = "application/json; charset=utf-8";
+            httpWebRequest.Method = "POST";
+
+            using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+            {
+                string json = "{\"GameType\":\"" + gameType + "\",\"" + "Joinable\":" + isJoinable + "," + "\"MaximumPlayers\":" + maxPlayers + "}";
+
+                streamWriter.Write(json);
+                streamWriter.Flush();
+            }
+            var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            {
+                var responseText = streamReader.ReadToEnd();
+                Console.WriteLine(responseText);
+
+                //Now you have your response.
+                //or false depending on information in the response     
+            }
+        }
+        catch (WebException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
+
+
