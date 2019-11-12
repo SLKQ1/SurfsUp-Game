98171dd5f37fd0a4085dcc9e4b2768cd10f23f16
Faiz Qureshi <faq588@mail.usask.ca>
2019-11-12 11:25:46 -0700

    Finished the create lobby canvas, still need to add functionality (talking with api)

---
+++ Assets/Scripts/LobbyUIScripts/CreateLobby.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+public class CreateLobby : MonoBehaviour
+{
+
+    List<string> gameTypeOptions = new List<string>() { "Please select","Solo", "Multiplayer" };
+    List<string> joinableOptions = new List<string>() { "Please select", "True", "False" };
+
+    public Dropdown gameTypeDropdown;
+    public Dropdown joinableDropdown;
+
+    public void Dropdown_IndexChanged_GameType(int index)
+    {
+        Debug.Log(gameTypeOptions[index]); 
+    }
+    public void Dropdown_IndexChanged_JoinableOptions(int index)
+    {
+        Debug.Log(joinableOptions[index]); 
+    }
+
+    private void Start()
+    {
+        PopulateLists(); 
+    }
+
+    void PopulateLists()
+    {
+        gameTypeDropdown.AddOptions(gameTypeOptions);
+        joinableDropdown.AddOptions(joinableOptions); 
+    }
+
+    // function for create lobby button 
+    public void OnClick()
+    {
+
+    }
+}
