f03341940a8d36023fffcb6de705c9d81d3001d5
Faiz Qureshi <faizqureshi111@gmail.com>
2019-11-14 12:05:38 -0700

    inLobby canvas now displays the current players and ready status

--- Assets/Scripts/LobbyUIScripts/InLobby.cs
+++ Assets/Scripts/LobbyUIScripts/InLobby.cs
@@ -1,26 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Net;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class InLobby : MonoBehaviour
 {
-    public Text curPlayers;
+    public Text curPlayersText;
     [SerializeField]
     private Text LobbyIDText;
     private int lobbyID;
-    // Start is called before the first frame update
+    //Start is called before the first frame update
+
     //void Start()
     //{
-    //    // display the current players in the lobby 
-    //    Debug.Log(lobbyID); 
+    //    InvokeRepeating("GetLobby", 0f, 5f);
+
     //}
 
-    //// Update is called once per frame
-    //void Update()
-    //{
-    //    // update the text to show all the current ready statuses 
-    //}
+    //Update is called once per frame
+    void Update()
+    {
+        curPlayersText.text = "\n"; 
+        // update the text to show all the current players and ready statuses
+        int[] curPlayers = GetLobby(lobbyID).CurrentPlayers;
+        for (int i = 0; i < curPlayers.Length; i++)
+        {
+            string curPlayerName = GetPlayer(curPlayers[i]).PlayerName;
+            bool curPlayerStatus = GetPlayer(curPlayers[i]).PlayerReady; 
+            curPlayersText.text += curPlayerName + " " + curPlayerStatus + "\n"; 
+        }
+
+
+    }
 
     public void SetLobbyID(int LobbyID)
     {
@@ -28,4 +40,17 @@ public class InLobby : MonoBehaviour
         lobbyID = LobbyID;
     }
 
+    private LobbyInfo GetLobby(int LobbyID)
+    {
+        JSONParser JSONParser = new JSONParser();
+        LobbyInfo curLobby = JSONParser.GetLobby(lobbyID);
+        return curLobby; 
+    }
+
+    private PlayerInfo GetPlayer(int playerID)
+    {
+        JSONParser JSONParser = new JSONParser();
+        PlayerInfo curPlayer = JSONParser.GetPlayer(playerID);
+        return curPlayer; 
+    }
 }
