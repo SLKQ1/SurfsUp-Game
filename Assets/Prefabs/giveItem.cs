b782a6651b9070899bce5d4203b14e9b885cf6ad
Ryan <rrhu98@gmail.com>
2019-11-18 04:01:36 -0600

    added script to give player random item

---
+++ Assets/Prefabs/giveItem.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class giveItem : MonoBehaviour
+{
+    public GameObject[] _itemPrefabs;
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            playerGet();
+        }
+    }
+
+    void playerGet()
+    {
+        int randomItem = Instantiate(_itemPrefabs[Random.Range(0,_itemPrefabs.Length)],transform.position,Quaternion.identity);
+        print("Player got "+  randomItem);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
