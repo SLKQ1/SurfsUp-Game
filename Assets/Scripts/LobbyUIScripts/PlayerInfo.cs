fa0e494faf2ad8e740dee889839c55d88d9b1d23
Faiz Qureshi <faizqureshi111@gmail.com>
2019-11-19 10:24:18 -0600

    port and Ip is now being set when lobby is full and players are all ready

--- Assets/Scripts/LobbyUIScripts/PlayerInfo.cs
+++ Assets/Scripts/LobbyUIScripts/PlayerInfo.cs
@@ -5,15 +5,6 @@ using Newtonsoft.Json;
 
 public class PlayerInfo 
 {
-    //    type player struct {
-
-    //    ID int    `json:"ID"`
-    //	Token string `json:"Token"`       // The Token of the player will serve as a secret between a player and the lobby service
-    //	LobbyID int    `json:"LobbyID"`     // The lobby the player is currently joined. 0 if player is not in any lobby.
-    //	PlayerName string `json:"PlayerName"`  // The name of the player shown in-game
-    //	PlayerTeam string `json:"PlayerTeam"`  // The team name or team color the player is currently on.
-    //	PlayerReady bool   `json:"PlayerReady"` // Whether or not the player is ready to play whilst in a lobby.
-    //}
     public int ID { get; set; }
     public string Token { get; set; }
     public int LobbyID { get; set; }
