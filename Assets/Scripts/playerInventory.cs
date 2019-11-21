62eed395eee40a4568a6f69e7b877176111656d6
Ryan <rrhu98@gmail.com>
2019-11-21 06:33:24 -0600

    player inventory and player place down item implemented
    however, placing is still bugged for some reason

---
+++ Assets/Scripts/playerInventory.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerInventory : MonoBehaviour
+{
+    public GameObject heldItem;
+
+    public void setItem(GameObject ob)
+    {
+        this.heldItem = ob;
+    }
+
+    public void UsedItem()
+    {
+        this.heldItem = null;
+    }
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.heldItem = null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (this.heldItem != null)
+            {
+                Instantiate(heldItem, transform.position, transform.rotation);
+                UsedItem();
+            }
+            else
+            {
+                print("You don't have any items!");
+            }
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            print("player currently holding " + heldItem);
+        }
+    }
+}
