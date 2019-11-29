58d501e9d10b617107dd4c66d5e34ef3709bbc20
Faiq Sohail <me@faiqsohail.com>
2019-11-28 22:32:18 -0600

    Revert "Merge branch 'master' into 'gametest'"
    This reverts merge request !44

--- Assets/Scripts/LobbyUIScripts/InLobby.cs
+++ Assets/Scripts/LobbyUIScripts/InLobby.cs
@@ -12,6 +12,7 @@ using UnityEngine.Networking;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using TMPro;
+using Mirror;
 
 public class InLobby : MonoBehaviour
 {
@@ -27,10 +28,13 @@ public class InLobby : MonoBehaviour
     private int[] curPlayers;
     // var to store lobby inorder to minimize API calls
     LobbyInfo curLobby;
-    //public GameObject networkObject;
+    public Mirror.NetworkManager NetworkManagerScript;
+    public TelepathyTransport TelepathyTransportScript;
+
     //Start is called before the first frame update
     void Start()
     {
+        
         InvokeRepeating("UpdateCurrentPlayers", 0f, 1f);
 
     }
@@ -38,10 +42,15 @@ public class InLobby : MonoBehaviour
     {
         if (AllPlayersReady())
         {
-            Set_Port_and_IP(this.lobbyID);
-			SceneManager.LoadScene(1);
+           
 
-            //networkObject.GetComponent<NetworkManagerHUD>().showGUI = true; 
+            Set_Port_and_IP(this.lobbyID);
+            SceneManager.LoadScene(1);
+            //Mirror.NetworkClient.Connect(NetworkManagerScript.networkAddress);
+            GameStateManager.maxPlayers = curPlayers.Length; 
+           
+            Mirror.NetworkManager networkObject = GameObject.FindGameObjectWithTag("NetworkObject").GetComponent<Mirror.NetworkManager>();
+            networkObject.StartClient(); 
 
         }
     }
@@ -122,6 +131,7 @@ public class InLobby : MonoBehaviour
 
                 var response = await httpClient.SendAsync(request);
             }
+            GameObject.FindGameObjectWithTag("NetworkObject").SendMessage("SetPlayerName", playerName); 
         }
 
 
