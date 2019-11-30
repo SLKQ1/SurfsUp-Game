387f7488b7f811b9aae679fca85e10b36ef5eeaa
Faiz Qureshi <faizqureshi111@gmail.com>
2019-11-30 07:29:12 -0600

    Performance issues with button list should be fixed, still need to test on lab computers

--- Assets/Scripts/LobbyUIScripts/ButtonListControl.cs
+++ Assets/Scripts/LobbyUIScripts/ButtonListControl.cs
@@ -2,62 +2,63 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Newtonsoft.Json;
-using UnityEngine.UI; 
+using UnityEngine.UI;
 public class ButtonListControl : MonoBehaviour
 {
     [SerializeField]
     private GameObject buttonTemplate;
-
-    // field to store buttons 
+    // feild for buttons
     private GameObject[] gameObjects;
+    // json parser 
+    private JSONParser LobbyParser = new JSONParser();
+    // storing old lobby size
+    private int prev_lobbyListSize;
 
     private void Start()
     {
         GenButtons();
-        InvokeRepeating("DestroyAllButtons", 0f, 5f); 
+        //InvokeRepeating("DestroyAllButtons", 0f, 5f); 
 
     }
-    private void LateUpdate()
+    private void Update()
     {
-        DestroyAllButtons();
-        GenButtons();
+        if (LobbyListChanged())
+        {
+            DestroyAllButtons();
+            GenButtons();
+        }
     }
 
-    public async void ButtonClicked(int LobbyID)
+    public void ButtonClicked(int LobbyID)
     {
         PlayerPatch newPlayerPatch = new PlayerPatch();
         // changing players lobby id 
         CurrentPlayer.curPlayer.LobbyID = LobbyID;
-        await newPlayerPatch.PatchPlayer(CurrentPlayer.curPlayer);
+        newPlayerPatch.PatchPlayer(CurrentPlayer.curPlayer);
 
-        // changing the panel to lobby instance 
-        LobbyManager.Instance.MainMenu.SetActive(false);
-        LobbyManager.Instance.lobbyListMenu.SetActive(false);
-        LobbyManager.Instance.createLobbyMenu.SetActive(false);
-        LobbyManager.Instance.createPlayerMenu.SetActive(false);
-        LobbyManager.Instance.inLobbyMenu.SetActive(true);
+        // changing the panel to lobby instance
+        LobbyManager.Instance.CreateInLobbyMenu();
 
     }
 
     // function to generate buttons
     void GenButtons()
     {
-        // testing to see if lobbies show
-        JSONParser LobbyParser = new JSONParser();
         List<LobbyInfo> LobbyList = LobbyParser.GetListLobbies();
-        //Debug.Log("lobby: " + LobbyList[index: 0].ID);
+        prev_lobbyListSize = LobbyList.Count;
 
         // loop to create lobby buttons 
         for (int i = 0; i < LobbyList.Count; i++)
         {
-            if (!LobbyList[i].IsStarted) {
+            if (!LobbyList[i].IsStarted)
+            {
                 // making button and making visible
                 GameObject button = Instantiate(buttonTemplate) as GameObject;
-                button.tag = "LobbyListButton"; 
+                button.tag = "LobbyListButton";
                 button.SetActive(true);
 
                 // setting button text to be the lobby id and if it is started or not
-                button.GetComponent<ButtonListButton>().SetText(LobbyList[i].ID,LobbyList[i].GameType + "\t" +LobbyList[i].CurrentPlayers.Length.ToString() + "/" + LobbyList[i].MaximumPlayers.ToString());
+                button.GetComponent<ButtonListButton>().SetText(LobbyList[i].ID, LobbyList[i].GameType + "\t" + LobbyList[i].CurrentPlayers.Length.ToString() + "/" + LobbyList[i].MaximumPlayers.ToString());
                 // setting button pos
                 button.transform.SetParent(buttonTemplate.transform.parent, false);
 
@@ -74,5 +75,14 @@ public class ButtonListControl : MonoBehaviour
         {
             Destroy(gameObjects[i]);
         }
+    }
+    private bool LobbyListChanged()
+    {
+        int new_lobbyListSize = LobbyParser.GetListLobbies().Count;
+        if (prev_lobbyListSize != new_lobbyListSize)
+        {
+            return true;
+        }
+        return false;
     }
 }
